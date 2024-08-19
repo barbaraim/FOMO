a164875283f922ab800be23d66928340e51181cf
Barbara Irarrazaval <birarrazaval@gmail.com>
2024-08-19 19:05:14 -0400

    feat: generate base events model and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_12_030538) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_19_230413) do
+  create_table "events", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,4 +38,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_12_030538) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users"
 end
