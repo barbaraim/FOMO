1faacfc0dde2784277b6ed8c5997c43abd0831b8
Barbara Irarrazaval <birarrazaval@gmail.com>
2024-08-31 22:34:48 -0400

    feat: filter events by time in all events view

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  scope :upcoming, -> { where("start_date >= ?", Date.today) }
+  scope :past, -> { where("end_date < ?", Date.today) }
+  scope :happening_now, -> { where("start_date <= ? AND end_date >= ?", Date.today, Date.today) }
+  scope :now_and_upcoming, -> { where("end_date >= ?", Date.today) }
 end
