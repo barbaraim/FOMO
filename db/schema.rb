823f8da7b4f13b7bbddcf3ba2eb4c6a1bac1b1df
Barbara Irarrazaval <birarrazaval@gmail.com>
2024-09-07 18:28:05 -0400

    feat: archive past events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_19_230413) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_07_215903) do
   create_table "events", force: :cascade do |t|
     t.string "name", null: false
     t.string "address"
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_19_230413) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "archived", default: false
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
