f98795faaa4f770a8ac99ee7c4564101850467bd
Barbara Irarrazaval <birarrazaval@gmail.com>
2024-09-21 17:53:12 -0300

    chore: add needed configuration for users endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
   resources :events
   resources :participants
   get "my_participations", action: :user_index, controller: "participants"
-  devise_for :users
+
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
