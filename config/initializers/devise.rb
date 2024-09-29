ec8a9eea2008cd22d9b398517330b6e1f1eccc37
Barbara Irarrazaval <birarrazaval@gmail.com>
2024-09-29 18:58:05 -0300

    fix: rubocop style errors

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -314,10 +314,10 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
     jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
+      [ "POST", %r{^/login$} ]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}]
+      [ "DELETE", %r{^/logout$} ]
     ]
     jwt.expiration_time = 120.minutes.to_i
   end
